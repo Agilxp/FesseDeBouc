51bd447b6b9422ec2048772b81d400e370be6208
Geoffrey Rekier <geoffrey.rekier@schibsted.no>
2024-11-29 14:32:43 +0100

    EP-48: WIP on new navigation

--- composeApp/src/commonMain/kotlin/com/agilxp/fessedebouc/App.kt
+++ composeApp/src/commonMain/kotlin/com/agilxp/fessedebouc/App.kt
@@ -1,27 +1,86 @@
 package com.agilxp.fessedebouc
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.agilxp.fessedebouc.ui.FesseDeBoucApp
 import com.agilxp.fessedebouc.ui.viewmodel.GroupViewModel
+import com.composables.icons.lucide.*
+
+val navigationItems = listOf(
+    mapOf("icon" to Lucide.Group, "label" to "Groups"),
+    mapOf("icon" to Lucide.MailCheck, "label" to "Invitations"),
+    mapOf("icon" to Lucide.CalendarCheck2, "label" to "Events"),
+    mapOf("icon" to Lucide.UserCog, "label" to "Settings"),
+)
 
 @Composable
 fun App(
     groupViewModel: GroupViewModel = viewModel { GroupViewModel() }
 ) {
     colors = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+    var navigationIndex by remember { mutableStateOf(0) }
     MaterialTheme(colorScheme = colors) {
         BoxWithConstraints {
-            if (maxWidth.value < 1000) {
-                Text("Coming Soon")
+            val smallScreen = maxWidth.value < 1000
+            if (smallScreen) {
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar(Modifier.fillMaxSize()) {
+                            navigationItems.forEachIndexed { index, map ->
+                                val item = navigationItems.get(index)
+                                val icon = item.get("icon") as ImageVector
+                                val label = item.get("label") as String
+                                NavigationBarItem(
+                                    selected = index == navigationIndex,
+                                    icon = { Icon(icon, label) },
+                                    label = { Text(label) },
+                                    onClick = { navigationIndex = index },
+                                    colors = NavigationBarItemDefaults.colors()
+                                )
+                            }
+                        }
+                    }
+                ) { contentPadding ->
+                    Text("small : $navigationIndex", Modifier.padding(contentPadding).fillMaxSize())
+                }
             } else {
-                FesseDeBoucApp(groupViewModel)
+                Scaffold { contentPadding ->
+                    PermanentNavigationDrawer(modifier = Modifier.fillMaxSize().background(colors.primaryContainer),drawerContent = {
+                        PermanentDrawerSheet(Modifier.width(200.dp).fillMaxSize()) {
+                            LazyColumn(Modifier.padding(vertical = 100.dp).fillMaxSize()) {
+                                itemsIndexed(navigationItems) { index, map ->
+                                    val item = navigationItems.get(index)
+                                    val icon = item.get("icon") as ImageVector
+                                    val label = item.get("label") as String
+                                    NavigationDrawerItem(
+                                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                                        selected = index == navigationIndex,
+                                        icon = { Icon(icon, label) },
+                                        label = { Text(label) },
+                                        onClick = { navigationIndex = index })
+                                }
+                            }
+                        }
+                    }, content = {
+                        when (navigationIndex) {
+                            0 -> Text("Groups")
+                            1 -> Text("Invitations")
+                            2 -> Text("Events")
+                            3 -> Text("Settings")
+                        }
+                    })
+                }
             }
         }
     }
--- composeApp/src/commonMain/kotlin/com/agilxp/fessedebouc/ui/FesseDeBoucApp.kt
+++ composeApp/src/commonMain/kotlin/com/agilxp/fessedebouc/ui/FesseDeBoucApp.kt
@@ -1,16 +1,10 @@
 package com.agilxp.fessedebouc.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.agilxp.fessedebouc.colors
@@ -34,23 +28,25 @@ fun FesseDeBoucApp(
         Row(
             modifier = Modifier.padding(innerPadding).fillMaxSize().height(1.dp), // Don't know why but height is needed
         ) {
-            Box(modifier = Modifier.fillMaxWidth(0.2f).fillMaxHeight()) {
-                GroupMenu(groupViewModel)
+            Box(modifier = Modifier.width(48.dp).fillMaxHeight()) {
             }
-            VerticalDivider(color = colors.primaryContainer, modifier = Modifier.fillMaxHeight().width(1.dp))
-            if (groupUiState.selectedGroup != null) {
-                Box(modifier = Modifier.fillMaxWidth(0.75f)) {
-                    MainWindow(groupViewModel)
-                }
-                VerticalDivider(color = colors.primaryContainer, modifier = Modifier.fillMaxHeight().width(1.dp))
-                Box(Modifier.fillMaxWidth().fillMaxHeight()) {
-                    GroupAdmin(groupViewModel)
-                }
-            } else {
-                Box(modifier = Modifier.fillMaxSize(), Alignment.Center) {
-                    Text("No group selected")
-                }
-            }
+//            Box(modifier = Modifier.fillMaxWidth(0.2f).fillMaxHeight()) {
+//                GroupMenu(groupViewModel)
+//            }
+//            VerticalDivider(color = colors.primaryContainer, modifier = Modifier.fillMaxHeight().width(1.dp))
+//            if (groupUiState.selectedGroup != null) {
+//                Box(modifier = Modifier.fillMaxWidth(0.75f)) {
+//                    MainWindow(groupViewModel)
+//                }
+//                VerticalDivider(color = colors.primaryContainer, modifier = Modifier.fillMaxHeight().width(1.dp))
+//                Box(Modifier.fillMaxWidth().fillMaxHeight()) {
+//                    GroupAdmin(groupViewModel)
+//                }
+//            } else {
+//                Box(modifier = Modifier.fillMaxSize(), Alignment.Center) {
+//                    Text("No group selected")
+//                }
+//            }
         }
     }
 }
